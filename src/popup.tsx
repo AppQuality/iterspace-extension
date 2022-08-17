c5ecd7da436bddeb4dcdc10a8ca20b66b0d1d14f
Iacopo Leardini <iacopo.lea@gmail.com>
2022-08-17 12:37:12 +0200

    wip: test desktopCapture

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,4 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('iterspaceExtensionRoot');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<div>Iterspace Browser Extension Popup</div>);
+
+const capture = () => {
+  console.log('capture screen');
+  chrome.runtime.sendMessage({action: "recordScreen"}, function(response) {
+    console.log(response);
+  });
+}
+
+root.render(
+  <div>
+    Iterspace Browser Extension Popup
+    <button onClick={capture}>capture</button>
+  </div>);
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -24,3 +24,14 @@ chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   }
   console.log('Extension successfully installed!');
 })
+
+chrome.runtime.onMessage.addListener(
+  async function(request, sender) {
+    if (request.action === "recordScreen") {
+      chrome.desktopCapture.chooseDesktopMedia(['screen', 'audio'], sender.tab, (streamId, options) => {
+        console.log('streamId', streamId);
+        console.log('streamOptions', options);
+      })
+    }
+  }
+);
