8003f521704037e2f54349d3223ed6f78a9d0c43
Iacopo Leardini <iacopo.lea@gmail.com>
2022-08-18 12:58:31 +0200

    wip: first test with audio

--- src/feature/Recorder.ts
+++ src/feature/Recorder.ts
@@ -1,40 +1,41 @@
 
 export class Recorder {
+  stream: MediaStream;
+  options: MediaRecorderOptions;
+  recordedChunks: Blob[];
+  mediaRecorder: MediaRecorder;
 
-    stream: MediaStream;
-    options: MediaRecorderOptions;
-    recordedChunks: Blob[];
-    constructor(stream: MediaStream) {
-        this.stream = stream;
-        this.options = { mimeType: "video/webm; codecs=vp9" };
-        this.recordedChunks = [];
-
+  constructor() {
+    if (!window.localStream) {
+      console.warn('there is no stream initialized, abort new Record initialization');
+      return;
     }
-
-    startRecording() {
-        const mediaRecorder = new MediaRecorder(this.stream, this.options);
-        mediaRecorder.ondataavailable = (event) => {
-            if (event.data.size > 0) {
-              this.recordedChunks.push(event.data);
-              console.log(this.recordedChunks);
-              this.download();
-            } else {
-              console.warn("event.data.size <= 0")
-            }
-          };
-        mediaRecorder.start();
-    }
-    
-    download = () => {
-        const blob = new Blob(this.recordedChunks, {
-          type: "video/webm"
-        });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        document.body.appendChild(a);
-        a.href = url;
-        a.download = "test.webm";
-        a.click();
-        window.URL.revokeObjectURL(url);
+    window.localStream;
+    this.options = { mimeType: "video/webm; codecs=h264" };
+    this.recordedChunks = [];
+  }
+  startRecording() {
+    this.mediaRecorder = new MediaRecorder(window.localStream, this.options);
+    this.mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        this.recordedChunks.push(event.data);
+        this.download();
+      } else {
+        console.warn("event.data.size <= 0")
       }
+    };
+    this.mediaRecorder.start();
+  }
+  download() {
+    const blob = new Blob(this.recordedChunks, {
+      type: "video/webm"
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = url;
+    a.download = "test.webm";
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -3,17 +3,32 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('iterspaceExtensionRoot');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 
-const capture = () => {
-  console.log('capture screen');
+const startRecording = () => {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {type:"recordScreen", pippo:"suca"}, function(response){
+    chrome.tabs.sendMessage(tabs[0].id, {type:"startRecording"}, function(response){
+      console.log("log response from popup",response);
+    });
+  });
+}
+const stopRecording = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type:"stopRecording", pippo:"suca"}, function(response){
       console.log("log response from popup",response);
     });
   });
 }
 
+const permissions = async () => {
+  const result = await window.navigator.permissions.query({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    name: 'microphone',
+  })
+  alert(result.state)
+}
 root.render(
   <div>
-    Iterspace Browser Extension Popup
-    <button onClick={capture}>capture</button>
+    <p>Popup</p>
+    <button onClick={startRecording}>start recording</button>
+    <button onClick={stopRecording}>stop recording</button>
   </div>);
