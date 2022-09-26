ae8eae8a0a7d65281effb916314751db4cfe137f
alexSp84 <alex1984@hotmail.it>
2022-09-26 12:54:09 +0200

    feat: get the list of audio devices

--- src/feature/ToggleAudioButton.tsx
+++ src/feature/ToggleAudioButton.tsx
@@ -1,10 +1,12 @@
 import {setStorageItem} from "../storage";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import micActive from "./assets/micActive.svg";
 import { IconButton } from "@appquality/unguess-design-system";
 import { StyledManageAudio } from "./_styles";
 
 export const ToggleAudioButton = ({audioStatus}: {audioStatus: AudioStatus}) => {
+  const [tracks, setTracks] = useState<MediaStreamTrack[] | undefined>();
+
   const activateAudio = () => {
     setStorageItem("audioStatus", "active");
   }
@@ -12,6 +14,23 @@ export const ToggleAudioButton = ({audioStatus}: {audioStatus: AudioStatus}) => 
     setStorageItem("audioStatus", "inactive");
   }
 
+  const getAudioStream = async () => {
+    const audioStream = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true
+      }, video: false
+    });
+    setTracks(audioStream.getAudioTracks());
+  }
+
+  const onChangeAudioTrack = () => {
+    // TODO
+  }
+
+  useEffect(() => {
+    getAudioStream();
+  }, []);
+  
   return <StyledManageAudio>
     <IconButton className="audio-btn" isBasic={false} isPill={false}>
       {audioStatus === 'active'
